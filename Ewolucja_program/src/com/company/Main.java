af2f2b436adcdaf5bb443db1c0ece84f3f65f79f
ketrab100 <bartis300@gmail.com>
2020-04-14 19:06:31 +0200

    a

---
+++ Ewolucja_program/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+abstract class Drapieznik {
+    int glod;
+    int predkosc;
+    int pozycja_x;
+    int pozycja_y;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
