f7a1dca70fea27cc90657051dd19c80525df67ad
ketrab100 <bartis300@gmail.com>
2020-05-11 17:19:48 +0200

    a

---
+++ Ewolucja/src/com/company/PRE1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Vector;
+
+public class PRE1 extends Predator {
+
+    public PRE1(int id, int positonX, int positonY) {
+        super(positonX, positonY);
+        this.name="WOLF ";
+        this.speed = 5;
+        this.age=0;
+        this.maxDelivery=50;
+        this.maxStomach=100;
+        this.stomach=100;
+    }
+
+}
