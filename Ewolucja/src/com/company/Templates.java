c79d432941f8b28bc18c981119d81de4402ce155
ketrab100 <bartis300@gmail.com>
2020-06-02 12:14:13 +0200

    aaaa

---
+++ Ewolucja/src/com/company/Templates.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Templates {
+    Animal Tiger  = new Predator(15,20,8,30,30,35,30);
+    Animal Wolf = new Predator(10,20,10,20,25,21,20);
+    Animal Snake = new Predator(1,10,3,4,15,10,9);
+    Animal Dog = new Predator(7,15,8,10,12,9,7);
+    Animal Cow = new Herbivore(20,20,4,25,50,25);
+    Animal Goat = new Herbivore(10,20,6,15,15,8);
+    Animal Sheep = new Herbivore(15,5,3,15,15,6);
+}
